abe37bc0c17ed00dd0d212218aced534c4069444
srdengi <marcaliagaborras@gmail.com>
2024-08-01 22:11:02 +0200

    first commit

---
+++ src/data/userData.js
@@ -0,0 +1,60 @@
+const userData = [
+  {
+    userName: "Marc Aliaga",
+    userDesc: "Backend & Web 3 developer | Cybersecurity ",
+    userPic: "https://avatars.githubusercontent.com/u/173827120?v=4",
+    userPicDark: "https://avatars.githubusercontent.com/u/173827120?v=4",
+  },
+];
+
+
+
+
+const socialLinks = [
+
+  {
+    socialName: "GitHub",
+    socialUl: "https://github.com/srdengi",
+    socialIcon: "fa6-brands:github-alt",
+  },
+  {
+    socialName: "Instagram",
+    socialUl: "https://www.instagram.com/srdengi/",
+    socialIcon: "ri:instagram-fill",
+  },
+  {
+    socialName: "X/Twitter",
+    socialUl: "https://x.com/srdengi_eth",
+    socialIcon: "prime:twitter",
+  },
+  {
+    socialName: "Telegram",
+    socialUl: "https://t.me/srdengi",
+    socialIcon: "mingcute:telegram-fill",
+  },
+];
+
+const userLinks = [
+    {
+        title:"Portafolio - Soon...",
+        link:"",
+        icon:"ant-design:code-filled"
+    },
+    {
+        title: "Send me a mail",
+        link: "mailto:marcaliagaborras@gmail.com",
+        icon:"mingcute:mail-fill"
+
+    },
+    {
+      title: "Send me a whastapp",
+      link: "https://wa.me/34681271020?text=Hi,%20I've%20seen%20you%20on%20your%20website%20and%20would%20like%20to%20connect.",
+      icon:"mingcute:whatsapp-fill"
+
+  },
+   
+    
+]
+
+
+export { userData, userLinks, socialLinks };
